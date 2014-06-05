4243f8dd3d94cb7048c7840c5dc16e04e5dc2f3b
Andrus Adamchik <andrus@objectstyle.com>
2014-06-05 15:25:12 +0300

    Support for char PK #10

---
+++ src/test/java/com/nhl/link/rest/unit/cayenne/E6.java
@@ -0,0 +1,9 @@
+package com.nhl.link.rest.unit.cayenne;
+
+import com.nhl.link.rest.unit.cayenne.auto._E6;
+
+public class E6 extends _E6 {
+
+    private static final long serialVersionUID = 1L; 
+
+}
