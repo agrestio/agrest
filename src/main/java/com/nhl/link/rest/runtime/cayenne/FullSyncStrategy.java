19fa11102b333c5aec6e2bbe830b907026ce7da2
atomashpolskiy <nordmann89@gmail.com>
2015-02-17 23:18:51 +0300

    'UpdateBuilder.onDeleteUnrelate()' for managing relationships #62

--- src/main/java/com/nhl/link/rest/runtime/cayenne/FullSyncStrategy.java
+++ src/main/java/com/nhl/link/rest/runtime/cayenne/FullSyncStrategy.java
@@ -1,24 +1,26 @@
 package com.nhl.link.rest.runtime.cayenne;
 
+import com.nhl.link.rest.EntityUpdate;
+import com.nhl.link.rest.ObjectMapper;
+import com.nhl.link.rest.runtime.cayenne.CayenneUpdateBuilder.ObjectRelator;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.cayenne.DataObject;
-
-import com.nhl.link.rest.EntityUpdate;
-import com.nhl.link.rest.ObjectMapper;
-import com.nhl.link.rest.runtime.cayenne.CayenneUpdateBuilder.ObjectRelator;
 
 /**
  * @since 1.7
  */
 class FullSyncStrategy<T> extends CreateOrUpdateStrategy<T> {
 
+	private boolean onDeleteUnrelate;
+
 	public FullSyncStrategy(CayenneUpdateResponse<T> response, ObjectRelator<T> relator, ResourceReader reader,
-			ObjectMapper<T> mapper, boolean idempotent) {
+			ObjectMapper<T> mapper, boolean idempotent, boolean onDeleteUnrelate) {
 		super(response, relator, reader, mapper, idempotent);
+
+		this.onDeleteUnrelate = onDeleteUnrelate;
 	}
 
 	@Override
@@ -27,7 +29,7 @@ class FullSyncStrategy<T> extends CreateOrUpdateStrategy<T> {
 		Map<Object, Collection<EntityUpdate>> keyMap = mutableKeyMap();
 		List<T> allObjects = reader.allItems(response);
 
-		List<DataObject> deletedObjects = new ArrayList<>();
+		List<T> deletedObjects = new ArrayList<>();
 
 		for (T o : allObjects) {
 			Object key = mapper.keyForObject(o);
@@ -35,14 +37,18 @@ class FullSyncStrategy<T> extends CreateOrUpdateStrategy<T> {
 			Collection<EntityUpdate> updates = keyMap.remove(key);
 
 			if (updates == null) {
-				deletedObjects.add((DataObject) o);
+				deletedObjects.add(o);
 			} else {
 				update(updates, o);
 			}
 		}
 
 		if (!deletedObjects.isEmpty()) {
-			response.getUpdateContext().deleteObjects(deletedObjects);
+			if (onDeleteUnrelate) {
+				unrelate(deletedObjects);
+			} else {
+				delete(deletedObjects);
+			}
 		}
 
 		// check leftovers - those correspond to objects missing in the DB or
