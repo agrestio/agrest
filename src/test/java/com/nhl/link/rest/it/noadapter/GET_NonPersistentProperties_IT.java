11a2d88ac76ec461488075fefe9c9bbd111f888b
Andrus Adamchik <andrus@objectstyle.com>
2015-04-01 11:26:11 +0300

    Simplify POJO metadata compilation: LR annotations #92
    * undoing annotated properties for Cayenne entities. Not ready to support those

--- src/test/java/com/nhl/link/rest/it/noadapter/GET_NonPersistentProperties_IT.java
+++ src/test/java/com/nhl/link/rest/it/noadapter/GET_NonPersistentProperties_IT.java
@@ -43,19 +43,6 @@ public class GET_NonPersistentProperties_IT extends JerseyTestOnDerby {
 	}
 
 	@Test
-	public void testGET_Root_Annotated() {
-		insert("e15", "long_id, name", "1, 'xxx'");
-		insert("e14", "e15_id, long_id, name", "1, 8, 'yyy'");
-
-		Response response1 = target("/e14").queryParam("include", "name").queryParam("include", "notSoPrettyName")
-				.request().get();
-		assertEquals(Status.OK.getStatusCode(), response1.getStatus());
-		assertEquals(
-				"{\"success\":true,\"data\":[{\"name\":\"yyy\",\"notSoPrettyName\":\"yyy_notpretty\"}],\"total\":1}",
-				response1.readEntity(String.class));
-	}
-
-	@Test
 	public void testGET_Related() {
 		insert("e15", "long_id, name", "1, 'xxx'");
 		insert("e14", "e15_id, long_id, name", "1, 8, 'yyy'");
