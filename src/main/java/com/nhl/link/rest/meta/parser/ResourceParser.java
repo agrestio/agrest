c1b293902a3c83e6fba88fd2577666af3e3bc8e3
Andrus Adamchik <andrus@objectstyle.com>
2016-03-16 14:33:19 -0400

    unused import

--- src/main/java/com/nhl/link/rest/meta/parser/ResourceParser.java
+++ src/main/java/com/nhl/link/rest/meta/parser/ResourceParser.java
@@ -8,7 +8,6 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
