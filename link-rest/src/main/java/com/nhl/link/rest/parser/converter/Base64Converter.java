43ea289300dd4a9bad912f5c68effc8f643d872d
Andrus Adamchik <andrus@objectstyle.com>
2016-05-06 14:37:09 -0400

    replacing Base64 parser from 'sun.misc.' with a hackish but supported JDK parser

--- link-rest/src/main/java/com/nhl/link/rest/parser/converter/Base64Converter.java
+++ link-rest/src/main/java/com/nhl/link/rest/parser/converter/Base64Converter.java
@@ -1,30 +1,30 @@
 package com.nhl.link.rest.parser.converter;
 
+import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.DatatypeConverter;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.nhl.link.rest.LinkRestException;
 
-import java.io.IOException;
-
-import javax.ws.rs.core.Response.Status;
-
 public class Base64Converter extends AbstractConverter {
 
-    private static final JsonValueConverter instance = new Base64Converter();
+	private static final JsonValueConverter instance = new Base64Converter();
 
 	public static JsonValueConverter converter() {
 		return instance;
 	}
 
-    @Override
-    protected Object valueNonNull(JsonNode node) {
+	@Override
+	protected Object valueNonNull(JsonNode node) {
 
-        if (!node.isTextual()) {
-            throw new LinkRestException(Status.BAD_REQUEST, "Expected textual value, got: " + node.asText());
-        }
-        try {
-            return new sun.misc.BASE64Decoder().decodeBuffer(node.asText());
-        } catch (IOException e) {
-            throw new LinkRestException(Status.BAD_REQUEST, "Failed to decode Base64 value: " + node.asText());
-        }
-    }
+		if (!node.isTextual()) {
+			throw new LinkRestException(Status.BAD_REQUEST, "Expected textual value, got: " + node.asText());
+		}
+		try {
+			// TODO: replace with Base64.Decoder when we upgrade to java 8
+			return DatatypeConverter.parseBase64Binary(node.asText());
+		} catch (IllegalArgumentException e) {
+			throw new LinkRestException(Status.BAD_REQUEST, "Failed to decode Base64 value: " + node.asText(), e);
+		}
+	}
 }
