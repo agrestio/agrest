0cbc104972b7942b8961ed4d7971614883bba834
Andrus Adamchik <andrus@objectstyle.com>
2016-05-27 21:06:18 +0300

    regenerating classes with Cayenne M3

--- link-rest/src/test/java/com/nhl/link/rest/it/fixture/cayenne/auto/_E20.java
+++ link-rest/src/test/java/com/nhl/link/rest/it/fixture/cayenne/auto/_E20.java
@@ -15,15 +15,6 @@ public abstract class _E20 extends CayenneDataObject {
 
     private static final long serialVersionUID = 1L; 
 
-    @Deprecated
-    public static final String AGE_PROPERTY = "age";
-    @Deprecated
-    public static final String DESCRIPTION_PROPERTY = "description";
-    @Deprecated
-    public static final String NAME_PROPERTY = "name";
-    @Deprecated
-    public static final String E21_PROPERTY = "e21";
-
     public static final String ID_PK_COLUMN = "id";
 
     public static final Property<Integer> AGE = new Property<Integer>("age");
