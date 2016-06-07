4c7b4d5a9d961df056a50ce84c401061a4fcc108
Andrus Adamchik <andrus@objectstyle.com>
2016-06-07 10:55:02 +0300

    cleaning up deprecated test api

--- link-rest/src/test/java/com/nhl/link/rest/it/noadapter/GET_Related_IT.java
+++ link-rest/src/test/java/com/nhl/link/rest/it/noadapter/GET_Related_IT.java
@@ -6,21 +6,16 @@ import javax.ws.rs.core.FeatureContext;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import com.nhl.link.rest.it.fixture.cayenne.E17;
-import com.nhl.link.rest.it.fixture.cayenne.E18;
-import com.nhl.link.rest.it.fixture.resource.E17Resource;
-import com.nhl.link.rest.it.fixture.resource.E18Resource;
 import org.apache.cayenne.map.DataMap;
 import org.apache.cayenne.map.ObjAttribute;
 import org.apache.cayenne.map.ObjEntity;
-import org.apache.cayenne.query.SQLTemplate;
 import org.junit.Test;
 
 import com.nhl.link.rest.it.fixture.JerseyTestOnDerby;
-import com.nhl.link.rest.it.fixture.cayenne.E12;
-import com.nhl.link.rest.it.fixture.cayenne.E2;
-import com.nhl.link.rest.it.fixture.cayenne.E3;
+import com.nhl.link.rest.it.fixture.cayenne.E18;
 import com.nhl.link.rest.it.fixture.resource.E12Resource;
+import com.nhl.link.rest.it.fixture.resource.E17Resource;
+import com.nhl.link.rest.it.fixture.resource.E18Resource;
 import com.nhl.link.rest.it.fixture.resource.E2Resource;
 import com.nhl.link.rest.it.fixture.resource.E3Resource;
 
@@ -73,15 +68,11 @@ public class GET_Related_IT extends JerseyTestOnDerby {
 	@Test
 	public void testGet_ValidRel_ToOne_CompoundId() {
 
-		performQuery(new SQLTemplate(E17.class, "INSERT INTO utest.e17 (id1, id2, name) values (1, 1, 'aaa')"));
-		performQuery(new SQLTemplate(E17.class, "INSERT INTO utest.e17 (id1, id2, name) values (2, 2, 'bbb')"));
-
-		performQuery(new SQLTemplate(E18.class,
-				"INSERT INTO utest.e18 (id, e17_id1, e17_id2, name) values (1, 1, 1, 'xxx')"));
-		performQuery(new SQLTemplate(E18.class,
-				"INSERT INTO utest.e18 (id, e17_id1, e17_id2, name) values (2, 1, 1, 'yyy')"));
-		performQuery(new SQLTemplate(E18.class,
-				"INSERT INTO utest.e18 (id, e17_id1, e17_id2, name) values (3, 2, 2, 'zzz')"));
+		insert("e17", "id1, id2, name", "1, 1, 'aaa'");
+		insert("e17", "id1, id2, name", "2, 2, 'bbb'");
+		insert("e18", "id, e17_id1, e17_id2, name", "1, 1, 1, 'xxx'");
+		insert("e18", "id, e17_id1, e17_id2, name", "2, 1, 1, 'yyy'");
+		insert("e18", "id, e17_id1, e17_id2, name", "3, 2, 2, 'zzz'");
 
 		Response r1 = target("/e18/1").queryParam("include", E18.E17.getName()).request().get();
 
@@ -101,15 +92,11 @@ public class GET_Related_IT extends JerseyTestOnDerby {
 		ObjAttribute unmappedAttribute = E17.getAttribute("id2");
 		E17.removeAttribute("id2");
 
-		performQuery(new SQLTemplate(E17.class, "INSERT INTO utest.e17 (id1, id2, name) values (1, 1, 'aaa')"));
-		performQuery(new SQLTemplate(E17.class, "INSERT INTO utest.e17 (id1, id2, name) values (2, 2, 'bbb')"));
-
-		performQuery(new SQLTemplate(E18.class,
-				"INSERT INTO utest.e18 (id, e17_id1, e17_id2, name) values (1, 1, 1, 'xxx')"));
-		performQuery(new SQLTemplate(E18.class,
-				"INSERT INTO utest.e18 (id, e17_id1, e17_id2, name) values (2, 1, 1, 'yyy')"));
-		performQuery(new SQLTemplate(E18.class,
-				"INSERT INTO utest.e18 (id, e17_id1, e17_id2, name) values (3, 2, 2, 'zzz')"));
+		insert("e17", "id1, id2, name", "1, 1, 'aaa'");
+		insert("e17", "id1, id2, name", "2, 2, 'bbb'");
+		insert("e18", "id, e17_id1, e17_id2, name", "1, 1, 1, 'xxx'");
+		insert("e18", "id, e17_id1, e17_id2, name", "2, 1, 1, 'yyy'");
+		insert("e18", "id, e17_id1, e17_id2, name", "3, 2, 2, 'zzz'");
 
 		Response r1 = target("/e18/1").queryParam("include", E18.E17.getName()).request().get();
 
@@ -127,11 +114,11 @@ public class GET_Related_IT extends JerseyTestOnDerby {
 		// make sure we have e3s for more than one e2 - this will help us
 		// confirm that relationship queries are properly filtered.
 
-		performQuery(new SQLTemplate(E2.class, "INSERT INTO utest.e2 (id, name) values (1, 'xxx')"));
-		performQuery(new SQLTemplate(E2.class, "INSERT INTO utest.e2 (id, name) values (2, 'yyy')"));
-		performQuery(new SQLTemplate(E3.class, "INSERT INTO utest.e3 (id, e2_id, name) values (7, 2, 'zzz')"));
-		performQuery(new SQLTemplate(E3.class, "INSERT INTO utest.e3 (id, e2_id, name) values (8, 1, 'yyy')"));
-		performQuery(new SQLTemplate(E3.class, "INSERT INTO utest.e3 (id, e2_id, name) values (9, 1, 'zzz')"));
+		insert("e2", "id, name", "1, 'xxx'");
+		insert("e2", "id, name", "2, 'yyy'");
+		insert("e3", "id, e2_id, name", "7, 2, 'zzz'");
+		insert("e3", "id, e2_id, name", "8, 1, 'yyy'");
+		insert("e3", "id, e2_id, name", "9, 1, 'zzz'");
 
 		Response r1 = target("/e2/1/e3s").queryParam("include", "id").request().get();
 
@@ -145,11 +132,11 @@ public class GET_Related_IT extends JerseyTestOnDerby {
 		// make sure we have e3s for more than one e2 - this will help us
 		// confirm that relationship queries are properly filtered.
 
-		performQuery(new SQLTemplate(E2.class, "INSERT INTO utest.e2 (id, name) values (1, 'xxx')"));
-		performQuery(new SQLTemplate(E2.class, "INSERT INTO utest.e2 (id, name) values (2, 'yyy')"));
-		performQuery(new SQLTemplate(E3.class, "INSERT INTO utest.e3 (id, e2_id, name) values (7, 2, 'zzz')"));
-		performQuery(new SQLTemplate(E3.class, "INSERT INTO utest.e3 (id, e2_id, name) values (8, 1, 'yyy')"));
-		performQuery(new SQLTemplate(E3.class, "INSERT INTO utest.e3 (id, e2_id, name) values (9, 1, 'zzz')"));
+		insert("e2", "id, name", "1, 'xxx'");
+		insert("e2", "id, name", "2, 'yyy'");
+		insert("e3", "id, e2_id, name", "7, 2, 'zzz'");
+		insert("e3", "id, e2_id, name", "8, 1, 'yyy'");
+		insert("e3", "id, e2_id, name", "9, 1, 'zzz'");
 
 		Response r1 = target("/e3/7/e2").queryParam("include", "id").request().get();
 
@@ -169,14 +156,14 @@ public class GET_Related_IT extends JerseyTestOnDerby {
 	@Test
 	public void testGET_ToManyJoin() {
 
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e12 (id) values (11)"));
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e12 (id) values (12)"));
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e13 (id) values (14)"));
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e13 (id) values (15)"));
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e13 (id) values (16)"));
+		insert("e12", "id", "11");
+		insert("e12", "id", "12");
+		insert("e13", "id", "14");
+		insert("e13", "id", "15");
+		insert("e13", "id", "16");
 
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e12_e13 (e12_id, e13_id) values (11, 14)"));
-		performQuery(new SQLTemplate(E12.class, "INSERT INTO utest.e12_e13 (e12_id, e13_id) values (12, 16)"));
+		insert("e12_e13", "e12_id, e13_id", "11, 14");
+		insert("e12_e13", "e12_id, e13_id", "12, 16");
 
 		// excluding ID - can't render multi-column IDs yet
 		Response r1 = target("/e12/12/e1213").queryParam("exclude", "id").queryParam("include", "e12")
