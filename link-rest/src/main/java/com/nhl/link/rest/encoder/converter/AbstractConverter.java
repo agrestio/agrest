a681dd6ad799d51bb66d1b01f85311d73220c009
Andrei Tomashpolskiy <nordmann89@gmail.com>
2016-07-20 12:57:27 +0300

    Travis config -- disable deployment of snapshots for PRs

--- link-rest/src/main/java/com/nhl/link/rest/encoder/converter/AbstractConverter.java
+++ link-rest/src/main/java/com/nhl/link/rest/encoder/converter/AbstractConverter.java
@@ -6,7 +6,7 @@ public abstract class AbstractConverter implements StringConverter {
 	@Override
 	public String asString(Object object) {
 		if (object == null) {
-			throw new NullPointerException("Null object");
+			throw new NullPointerException("Null object ");
 		}
 		
 		return asStringNonNull(object);
