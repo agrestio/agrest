61a280fdf6bcb0362c674fce6d7ef3d7e1b691e6
Andrus Adamchik <andrus@objectstyle.com>
2017-06-30 14:38:45 -0400

    turning processing stage into a functional interface

--- link-rest/src/main/java/com/nhl/link/rest/processor/ProcessingStage.java
+++ link-rest/src/main/java/com/nhl/link/rest/processor/ProcessingStage.java
@@ -4,22 +4,25 @@ import java.lang.annotation.Annotation;
 
 /**
  * A single stage in a chained request processor.
- * 
+ *
  * @since 1.16
  */
 public interface ProcessingStage<C extends ProcessingContext<T>, T> {
 
-	/**
-	 * Executes this stage, returning the next stage to be executed or null if
-	 * the chain should be terminated.
-	 */
-	ProcessingStage<C, ? super T> execute(C context);
+    /**
+     * Executes this stage, returning the next stage to be executed or null if
+     * the chain should be terminated.
+     */
+    ProcessingStage<C, ? super T> execute(C context);
 
-	/**
-	 * Returns an Annotation class that can be used by listeners to annotate
-	 * their methods that should be invoked after the stage successfully
-	 * finishes execution. If the stage doesn't support listeners, this method
-	 * should return null.
-	 */
-	Class<? extends Annotation> afterStageListener();
+    /**
+     * Returns an Annotation class that can be used by listeners to annotate their methods that should be invoked after
+     * the stage successfully finishes execution. If the stage doesn't support listeners, this method should return null.
+     * Default implementation returns null.
+     *
+     * @return this implementation returns null.
+     */
+    default Class<? extends Annotation> afterStageListener() {
+        return null;
+    }
 }
