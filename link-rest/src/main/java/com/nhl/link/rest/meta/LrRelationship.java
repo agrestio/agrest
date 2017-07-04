fb8ebe92ddb74e0dd4920ce33a1f4b6795110fa4
Andrus Adamchik <andrus@objectstyle.com>
2017-07-04 21:23:16 +0300

    Remove API deprecated on or before 2.0 #244

--- link-rest/src/main/java/com/nhl/link/rest/meta/LrRelationship.java
+++ link-rest/src/main/java/com/nhl/link/rest/meta/LrRelationship.java
@@ -8,15 +8,6 @@ public interface LrRelationship {
 	String getName();
 
 	/**
-	 * @deprecated since 2.0 in favor of {@link #getTargetEntity()} that can
-	 *             provide the type.
-	 */
-	@Deprecated
-	default Class<?> getTargetEntityType() {
-		return getTargetEntity().getType();
-	}
-
-	/**
 	 * @since 2.0
 	 */
 	LrEntity<?> getTargetEntity();
