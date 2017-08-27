90548cb413286436314419a3dc80c5c095a8dbc3
Andrus Adamchik <andrus@objectstyle.com>
2017-08-27 14:54:30 +0300

    Java 8 refactoring

--- link-rest/src/main/java/com/nhl/link/rest/parser/converter/Normalizer.java
+++ link-rest/src/main/java/com/nhl/link/rest/parser/converter/Normalizer.java
@@ -7,33 +7,30 @@ import java.util.Map;
  * A utility class that ensures that client-provided numeric values are
  * converted to the right numeric type. This is especially important for ID
  * values that are used in comparisons.
- * 
+ *
  * @since 1.12
  */
 public class Normalizer {
 
-	static interface TypeNormalizer {
+    interface TypeNormalizer {
 		Object normalize(Object value);
 	}
 
 	@SuppressWarnings("serial")
 	private static final Map<Class<?>, TypeNormalizer> NORMALIZERS = new HashMap<Class<?>, TypeNormalizer>() {
 		{
-			put(Long.class, new TypeNormalizer() {
-				@Override
-				public Object normalize(Object value) {
+			put(Long.class, value -> {
 
-					if (!(value instanceof Number)) {
-						return value;
-					}
+                if (!(value instanceof Number)) {
+                    return value;
+                }
 
-					if (value instanceof Long) {
-						return value;
-					}
+                if (value instanceof Long) {
+                    return value;
+                }
 
-					return ((Number) value).longValue();
-				}
-			});
+                return ((Number) value).longValue();
+            });
 		}
 	};
 
