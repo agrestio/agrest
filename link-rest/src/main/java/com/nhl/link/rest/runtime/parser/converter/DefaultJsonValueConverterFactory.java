5f1f7d30e2cafcf44e6537689a8ae57d2b040665
Andrus Adamchik <andrus@objectstyle.com>
2017-08-27 15:00:10 +0300

    Java 8 refactoring - moving derived IJsonValueConverterFactory methods to the interface

--- link-rest/src/main/java/com/nhl/link/rest/runtime/parser/converter/DefaultJsonValueConverterFactory.java
+++ link-rest/src/main/java/com/nhl/link/rest/runtime/parser/converter/DefaultJsonValueConverterFactory.java
@@ -1,7 +1,5 @@
 package com.nhl.link.rest.runtime.parser.converter;
 
-import com.nhl.link.rest.meta.LrAttribute;
-import com.nhl.link.rest.meta.LrEntity;
 import com.nhl.link.rest.parser.converter.Base64Converter;
 import com.nhl.link.rest.parser.converter.FloatConverter;
 import com.nhl.link.rest.parser.converter.GenericConverter;
@@ -24,48 +22,33 @@ import java.util.Map;
  */
 public class DefaultJsonValueConverterFactory implements IJsonValueConverterFactory {
 
-	protected Map<Class<?>, JsonValueConverter> convertersByJavaType;
+    protected Map<Class<?>, JsonValueConverter> convertersByJavaType;
 
-	private JsonValueConverter defaultConverter;
+    private JsonValueConverter defaultConverter;
 
-	public DefaultJsonValueConverterFactory() {
+    public DefaultJsonValueConverterFactory() {
 
-		this.defaultConverter = GenericConverter.converter();
+        this.defaultConverter = GenericConverter.converter();
 
-		this.convertersByJavaType = new HashMap<>();
-		convertersByJavaType.put(Object.class, GenericConverter.converter());
-		convertersByJavaType.put(Float.class, FloatConverter.converter());
-		convertersByJavaType.put(float.class, FloatConverter.converter());
-		convertersByJavaType.put(Long.class, LongConverter.converter());
-		convertersByJavaType.put(long.class, LongConverter.converter());
-		convertersByJavaType.put(Date.class, UtcDateConverter.converter());
-		convertersByJavaType.put(java.sql.Date.class, UtcDateConverter.converter());
-		convertersByJavaType.put(java.sql.Time.class, UtcDateConverter.converter());
-		convertersByJavaType.put(java.sql.Timestamp.class, UtcDateConverter.converter());
-		convertersByJavaType.put(byte[].class, Base64Converter.converter());
-		convertersByJavaType.put(LocalDate.class, ISOLocalDateConverter.converter());
-		convertersByJavaType.put(LocalTime.class, ISOLocalTimeConverter.converter());
-		convertersByJavaType.put(LocalDateTime.class, ISOLocalDateTimeConverter.converter());
-	}
+        this.convertersByJavaType = new HashMap<>();
+        convertersByJavaType.put(Object.class, GenericConverter.converter());
+        convertersByJavaType.put(Float.class, FloatConverter.converter());
+        convertersByJavaType.put(float.class, FloatConverter.converter());
+        convertersByJavaType.put(Long.class, LongConverter.converter());
+        convertersByJavaType.put(long.class, LongConverter.converter());
+        convertersByJavaType.put(Date.class, UtcDateConverter.converter());
+        convertersByJavaType.put(java.sql.Date.class, UtcDateConverter.converter());
+        convertersByJavaType.put(java.sql.Time.class, UtcDateConverter.converter());
+        convertersByJavaType.put(java.sql.Timestamp.class, UtcDateConverter.converter());
+        convertersByJavaType.put(byte[].class, Base64Converter.converter());
+        convertersByJavaType.put(LocalDate.class, ISOLocalDateConverter.converter());
+        convertersByJavaType.put(LocalTime.class, ISOLocalTimeConverter.converter());
+        convertersByJavaType.put(LocalDateTime.class, ISOLocalDateTimeConverter.converter());
+    }
 
-	@Override
-	public JsonValueConverter converter(Class<?> valueType) {
-		JsonValueConverter converter = convertersByJavaType.get(valueType);
-		return converter != null ? converter : defaultConverter;
-	}
-
-	@Override
-	public JsonValueConverter converter(LrAttribute attribute) {
-		return converter(attribute.getType());
-	}
-
-	@Override
-	public JsonValueConverter converter(LrEntity<?> entity) {
-		int ids = entity.getIds().size();
-		if (ids != 1) {
-			throw new IllegalArgumentException("Entity '" + entity.getName() +
-					"' has unexpected number of ID attributes: " + ids);
-		}
-		return converter(entity.getIds().iterator().next().getType());
-	}
+    @Override
+    public JsonValueConverter converter(Class<?> valueType) {
+        JsonValueConverter converter = convertersByJavaType.get(valueType);
+        return converter != null ? converter : defaultConverter;
+    }
 }
--- link-rest/src/main/java/com/nhl/link/rest/runtime/parser/converter/IJsonValueConverterFactory.java
+++ link-rest/src/main/java/com/nhl/link/rest/runtime/parser/converter/IJsonValueConverterFactory.java
@@ -7,26 +7,35 @@ import com.nhl.link.rest.parser.converter.JsonValueConverter;
 /**
  * A service that ensures proper conversion of incoming JSON values to the
  * model-compatible Java types.
- * 
+ *
  * @since 1.10
  */
 public interface IJsonValueConverterFactory {
 
-	/**
-	 * @since 1.24
-	 */
-	JsonValueConverter converter(Class<?> valueType);
-
-	/**
-	 * @since 2.5
+    /**
+     * @since 1.24
      */
-	JsonValueConverter converter(LrAttribute attribute);
+    JsonValueConverter converter(Class<?> valueType);
 
-	/**
-	 * Get converter for entity ID.
-	 *
-	 * @throws IllegalArgumentException if entity has multiple IDs
-	 * @since 2.5
+    /**
+     * @since 2.5
      */
-	JsonValueConverter converter(LrEntity<?> entity);
+    default JsonValueConverter converter(LrAttribute attribute) {
+        return converter(attribute.getType());
+    }
+
+    /**
+     * Get converter for entity ID.
+     *
+     * @throws IllegalArgumentException if entity has multiple IDs
+     * @since 2.5
+     */
+    default JsonValueConverter converter(LrEntity<?> entity) {
+        int ids = entity.getIds().size();
+        if (ids != 1) {
+            throw new IllegalArgumentException("Entity '" + entity.getName() +
+                    "' has unexpected number of ID attributes: " + ids);
+        }
+        return converter(entity.getIds().iterator().next().getType());
+    }
 }
