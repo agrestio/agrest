901626b6a25c7f4cb03c0272335ab28e30d0ff07
Andrus Adamchik <andrus@objectstyle.com>
2017-09-07 10:11:55 -0400

    link-rest-sencha module #263
    * default implementation of the ModuleProvider methods

--- link-rest/src/main/java/com/nhl/link/rest/LrModuleProvider.java
+++ link-rest/src/main/java/com/nhl/link/rest/LrModuleProvider.java
@@ -1,7 +1,11 @@
 package com.nhl.link.rest;
 
 import com.nhl.link.rest.runtime.LinkRestRuntime;
+import org.apache.cayenne.di.Module;
 import org.apache.cayenne.di.spi.ModuleProvider;
+
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Provider of extension modules for {@link LinkRestRuntime}. Used either directly or via {@link java.util.ServiceLoader}
@@ -11,4 +15,9 @@ import org.apache.cayenne.di.spi.ModuleProvider;
  */
 public interface LrModuleProvider extends ModuleProvider {
 
+
+    @Override
+    default Collection<Class<? extends Module>> overrides() {
+        return Collections.emptySet();
+    }
 }
