825cf6d52402cc10c87e53602397f81cf1852f9c
Andrei Tomashpolskiy <nordmann89@gmail.com>
2017-10-16 14:05:27 +0300

    Create link-rest-base for common artifacts

---
+++ link-rest-base/src/main/java/com/nhl/link/rest/runtime/parser/converter/IJsonValueConverterFactory.java
@@ -0,0 +1,17 @@
+package com.nhl.link.rest.runtime.parser.converter;
+
+import com.nhl.link.rest.parser.converter.JsonValueConverter;
+
+/**
+ * A service that ensures proper conversion of incoming JSON values to the
+ * model-compatible Java types.
+ *
+ * @since 1.10
+ */
+public interface IJsonValueConverterFactory {
+
+    /**
+     * @since 1.24
+     */
+    JsonValueConverter converter(Class<?> valueType);
+}
