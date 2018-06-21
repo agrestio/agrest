3c4ebb5bc03b9dc82066931c92393960650242ec
Andrus Adamchik <andrus@objectstyle.com>
2018-06-21 11:20:47 +0300

    Move "query" protocol parameter to "link-rest-sencha" #301
    * allowing both PARSE_REQUEST and ASSEMBLE_QUERY stages
    
    (cherry picked from commit 13406a501773feaa44dbc5be5bc431a62cd7e570)

--- link-rest-sencha/src/main/java/com/nhl/link/rest/sencha/SenchaOps.java
+++ link-rest-sencha/src/main/java/com/nhl/link/rest/sencha/SenchaOps.java
@@ -3,6 +3,7 @@ package com.nhl.link.rest.sencha;
 import com.nhl.link.rest.runtime.parser.BaseRequestProcessor;
 import com.nhl.link.rest.runtime.processor.select.SelectContext;
 import com.nhl.link.rest.sencha.ops.StartsWithFilter;
+import org.apache.cayenne.exp.Expression;
 import org.apache.cayenne.exp.Property;
 
 import javax.ws.rs.core.UriInfo;
@@ -26,6 +27,20 @@ public class SenchaOps {
         return c -> StartsWithFilter
                 .getInstance()
                 .filter(c, property.getName(), value)
-                .ifPresent(c.getSelect()::andQualifier);
+                .ifPresent(exp -> processStartsWith(c, exp));
+    }
+
+    private static void processStartsWith(SelectContext<?> context, Expression expression) {
+
+        // we can be called from different stages of the pipeline. Context may or may not have all the pieces in place..
+
+        if (context.getEntity() != null) {
+            context.getEntity().andQualifier(expression);
+        }
+        // else - error?
+
+        if (context.getSelect() != null) {
+            context.getSelect().andQualifier(expression);
+        }
     }
 }
