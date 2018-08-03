5dacaa93464db1026df4adaf9aaa30ea529f54f2
Andrus Adamchik <andrus@objectstyle.com>
2018-08-03 16:08:55 +0300

    Integration tests for built-in exception mappers

---
+++ link-rest/src/test/java/com/nhl/link/rest/provider/CayenneRuntimeExceptionMapperIT.java
@@ -0,0 +1,53 @@
+package com.nhl.link.rest.provider;
+
+import com.nhl.link.rest.DataResponse;
+import com.nhl.link.rest.it.fixture.JerseyTestOnDerby;
+import com.nhl.link.rest.it.fixture.cayenne.E2;
+import org.apache.cayenne.CayenneException;
+import org.apache.cayenne.CayenneRuntimeException;
+import org.junit.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+public class CayenneRuntimeExceptionMapperIT extends JerseyTestOnDerby {
+
+    @Override
+    protected void doAddResources(FeatureContext context) {
+        context.register(Resource.class);
+    }
+
+    @Test
+    public void testException() {
+        Response response = target("/g1").request().get();
+
+        String cayenneVersion = CayenneException.getExceptionLabel();
+        String expected = String
+                .format("{\"success\":false,\"message\":\"CayenneRuntimeException %s_something_w_cayenne_\"}", cayenneVersion);
+        onResponse(response)
+                .statusEquals(Response.Status.INTERNAL_SERVER_ERROR)
+                .bodyEquals(expected);
+    }
+
+    @Path("")
+    @Produces(MediaType.APPLICATION_JSON)
+    public static class Resource {
+
+        @Context
+        private Configuration config;
+
+        @GET
+        @Path("g1")
+        public DataResponse<E2> getE2(@Context UriInfo uriInfo) {
+            throw new CayenneRuntimeException("_something_w_cayenne_");
+        }
+    }
+}
+
