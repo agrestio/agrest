0fd5d5152b2414a1c7ac50197f0f20776877f168
Victor Yarmolovich <vyarmolovich@gmail.com>
2019-01-31 13:52:00 +0300

    #387 Refactored Idempotent stage to fetch items using separate requests.

--- agrest/src/main/java/io/agrest/runtime/cayenne/processor/update/CayenneIdempotentFullSyncStage.java
+++ agrest/src/main/java/io/agrest/runtime/cayenne/processor/update/CayenneIdempotentFullSyncStage.java
@@ -1,18 +1,28 @@
 package io.agrest.runtime.cayenne.processor.update;
 
 import io.agrest.AgException;
+import io.agrest.CompoundObjectId;
 import io.agrest.EntityUpdate;
 import io.agrest.ObjectMapper;
+import io.agrest.ResourceEntity;
+import io.agrest.SimpleObjectId;
+import io.agrest.meta.AgAttribute;
+import io.agrest.meta.AgPersistentEntity;
+import io.agrest.meta.AgRelationship;
 import io.agrest.runtime.meta.IMetadataService;
 import io.agrest.runtime.processor.update.UpdateContext;
 import org.apache.cayenne.DataObject;
 import org.apache.cayenne.di.Inject;
+import org.apache.cayenne.exp.Expression;
+import org.apache.cayenne.exp.ExpressionFactory;
+import org.apache.cayenne.exp.Property;
 import org.apache.cayenne.map.EntityResolver;
 import org.apache.cayenne.query.SelectQuery;
 
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -57,23 +67,15 @@ public class CayenneIdempotentFullSyncStage extends CayenneIdempotentCreateOrUpd
     }
 
     <T extends DataObject> List<T> allItems(UpdateContext<T> context) {
-        SelectQuery<T> query = SelectQuery.query(context.getType());
+        SelectQuery<T> query = buildQuery(context, context.getEntity());
 
-        // apply various request filters identifying the span of the collection
-
-        if (context.getParent() != null) {
-            EntityResolver resolver = CayenneUpdateStartStage.cayenneContext(context).getEntityResolver();
-            query.andQualifier(context.getParent().qualifier(resolver));
-        }
-
-        if (context.getEntity().getQualifier() != null) {
-            query.andQualifier(context.getEntity().getQualifier());
-        }
 
         // TODO: use SelectBuilder to get Cayenne representation of the
         // resource, instead of duplicating this here...
 
-        List<T> objects = CayenneUpdateStartStage.cayenneContext(context).select(query);
+        // List<T> objects = CayenneUpdateStartStage.cayenneContext(context).select(query);
+        List<T> objects = fetchEntity(context, context.getEntity());
+
         if (context.isById() && objects.size() > 1) {
             throw new AgException(Response.Status.INTERNAL_SERVER_ERROR, String.format(
                     "Found more than one object for ID '%s' and entity '%s'",
@@ -82,6 +84,103 @@ public class CayenneIdempotentFullSyncStage extends CayenneIdempotentCreateOrUpd
         return objects;
     }
 
+    <T> SelectQuery<T> buildQuery(UpdateContext<T> context, ResourceEntity<T> entity) {
 
+        SelectQuery<T> query = SelectQuery.query(entity.getAgEntity().getType());
+
+        // apply various request filters identifying the span of the collection
+
+        if (context.getParent() != null) {
+            EntityResolver resolver = CayenneUpdateStartStage.cayenneContext(context).getEntityResolver();
+            query.andQualifier(context.getParent().qualifier(resolver));
+        }
+
+        if (entity.getQualifier() != null) {
+            query.andQualifier(entity.getQualifier());
+        }
+
+        entity.setSelect(query);
+
+        buildChildrenQuery(context, entity, entity.getChildren());
+
+        return query;
+    }
+
+    private void buildChildrenQuery(UpdateContext context, ResourceEntity<?> entity, Map<String, ResourceEntity<?>> children) {
+        if (!children.isEmpty()) {
+            for (Map.Entry<String, ResourceEntity<?>> e : children.entrySet()) {
+                ResourceEntity child  = e.getValue();
+                if (!(child.getAgEntity() instanceof AgPersistentEntity)) {
+                    continue;
+                }
+
+                List<Property> properties = new ArrayList<>();
+                properties.add(Property.createSelf(child.getType()));
+
+                AgRelationship relationship = child.getAgEntity().getRelationship(entity.getAgEntity());
+                if (relationship != null) {
+                    for (AgAttribute attribute : (Collection<AgAttribute>) entity.getAgEntity().getIds()) {
+                        properties.add(Property.create(ExpressionFactory.dbPathExp(relationship.getName() + "." + attribute.getName()), (Class) attribute.getType()));
+                    }
+                    // transfer expression from parent
+                    if (entity.getSelect().getQualifier() != null) {
+                        child.andQualifier((Expression) relationship.translateExpressionToTarget(entity.getSelect().getQualifier()));
+                    }
+
+                }
+
+                SelectQuery childQuery = buildQuery(context, child);
+                childQuery.setColumns(properties);
+            }
+        }
+    }
+
+
+    protected <T> List<T>  fetchEntity(UpdateContext<T> context, ResourceEntity<T> resourceEntity) {
+        SelectQuery<T> select = resourceEntity.getSelect();
+
+        List<T> objects = CayenneUpdateStartStage.cayenneContext(context).select(select);
+
+        fetchChildren(context, resourceEntity, resourceEntity.getChildren());
+
+        return objects;
+    }
+
+    protected <T> void fetchChildren(UpdateContext context, ResourceEntity<T> parent, Map<String, ResourceEntity<?>> children) {
+        if (!children.isEmpty()) {
+            for (Map.Entry<String, ResourceEntity<?>> e : children.entrySet()) {
+                ResourceEntity childEntity = e.getValue();
+
+                List childObjects = fetchEntity(context, childEntity);
+
+                assignChildrenToParent(parent, childEntity, childObjects);
+            }
+        }
+    }
+
+    /**
+     * Assigns child items to the appropriate parent item
+     */
+    protected <T> void assignChildrenToParent(ResourceEntity<T> parentEntity, ResourceEntity childEntity, List children) {
+        // saves a result
+        for (Object child : children) {
+            if (child instanceof Object[]) {
+                Object[] ids = (Object[])child;
+                if (ids.length == 2) {
+                    childEntity.addToResult( new SimpleObjectId(ids[1]), (T) ids[0]);
+                } else if (ids.length > 2) {
+                    // saves entity with a compound ID
+                    Map<String, Object> compoundKeys = new LinkedHashMap<>();
+                    AgAttribute[] idAttributes = parentEntity.getAgEntity().getIds().toArray(new AgAttribute[0]);
+                    if (idAttributes.length == (ids.length - 1)) {
+                        for (int i = 1; i < ids.length; i++) {
+                            compoundKeys.put(idAttributes[i - 1].getName(), ids[i]);
+                        }
+                    }
+                    childEntity.addToResult( new CompoundObjectId(compoundKeys), (T) ids[0]);
+                }
+            }
+        }
+    }
 
 }
