a7d9d6724e31abc829f9c4120b063600b9b089eb
Andrus Adamchik <andrus@objectstyle.com>
2019-05-15 16:59:04 +0300

    formatting, no change

--- agrest/src/main/java/io/agrest/DataResponseFlattenExtractor.java
+++ agrest/src/main/java/io/agrest/DataResponseFlattenExtractor.java
@@ -1,86 +1,84 @@
 package io.agrest;
 
+import io.agrest.encoder.Encoder;
+import io.agrest.encoder.EncoderVisitor;
+
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.regex.Pattern;
 
-import io.agrest.encoder.Encoder;
-import io.agrest.encoder.EncoderVisitor;
-
 class DataResponseFlattenExtractor<U> implements EncoderVisitor {
 
-	static enum State {
-		matching, collecting, invalid
-	}
+    private static final Pattern SPLIT_PATH = Pattern.compile("\\.");
+    private Collection<U> result;
+    private String[] path;
+    private Deque<String> stack;
+    private State state;
+    public DataResponseFlattenExtractor(String path) {
+        this.path = path == null || path.length() == 0 ? new String[0] : SPLIT_PATH.split(path);
+        this.result = new ArrayList<>();
+        this.state = path.length() > 0 ? State.matching : State.collecting;
+        this.stack = new ArrayDeque<>();
+    }
 
-	private static final Pattern SPLIT_PATH = Pattern.compile("\\.");
+    public Collection<U> getResult() {
+        return result;
+    }
 
-	private Collection<U> result;
-	private String[] path;
-	private Deque<String> stack;
-	private State state;
+    @SuppressWarnings("unchecked")
+    @Override
+    public int visit(Object object) {
 
-	public DataResponseFlattenExtractor(String path) {
-		this.path = path == null || path.length() == 0 ? new String[0] : SPLIT_PATH.split(path);
-		this.result = new ArrayList<>();
-		this.state = path.length() > 0 ? State.matching : State.collecting;
-		this.stack = new ArrayDeque<>();
-	}
+        switch (state) {
+            case matching:
+                return Encoder.VISIT_CONTINUE;
+            case collecting:
+                result.add((U) object);
+                return Encoder.VISIT_SKIP_CHILDREN;
+            case invalid:
+                return Encoder.VISIT_SKIP_CHILDREN;
+            default:
+                return Encoder.VISIT_SKIP_CHILDREN;
+        }
+    }
 
-	public Collection<U> getResult() {
-		return result;
-	}
+    @Override
+    public void push(String relationship) {
+        stack.push(relationship);
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public int visit(Object object) {
+        switch (state) {
+            case matching:
+                if (path[stack.size() - 1].equals(relationship)) {
 
-		switch (state) {
-		case matching:
-			return Encoder.VISIT_CONTINUE;
-		case collecting:
-			result.add((U) object);
-			return Encoder.VISIT_SKIP_CHILDREN;
-		case invalid:
-			return Encoder.VISIT_SKIP_CHILDREN;
-		default:
-			return Encoder.VISIT_SKIP_CHILDREN;
-		}
-	}
+                    if (path.length == stack.size()) {
+                        state = State.collecting;
+                    }
+                } else {
+                    state = State.invalid;
+                }
 
-	@Override
-	public void push(String relationship) {
-		stack.push(relationship);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected state on push: " + state);
+        }
+    }
 
-		switch (state) {
-		case matching:
-			if (path[stack.size() - 1].equals(relationship)) {
+    @Override
+    public void pop() {
+        stack.pop();
 
-				if (path.length == stack.size()) {
-					state = State.collecting;
-				}
-			} else {
-				state = State.invalid;
-			}
+        switch (state) {
+            case collecting:
+                state = State.matching;
+                break;
+            default:
+                break;
+        }
+    }
 
-			break;
-		default:
-			throw new IllegalStateException("Unexpected state on push: " + state);
-		}
-	}
-
-	@Override
-	public void pop() {
-		stack.pop();
-
-		switch (state) {
-		case collecting:
-			state = State.matching;
-			break;
-		default:
-			break;
-		}
-	}
+    enum State {
+        matching, collecting, invalid
+    }
 }
