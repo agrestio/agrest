efef8a50b13b5c956ec058724c4703fbb1e39f56
Andrus Adamchik <andrus@objectstyle.com>
2019-08-29 08:55:48 +0300

    cleanup, no change

--- agrest/src/main/java/io/agrest/encoder/FilterChainEncoder.java
+++ agrest/src/main/java/io/agrest/encoder/FilterChainEncoder.java
@@ -1,63 +1,58 @@
 package io.agrest.encoder;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+
 import java.io.IOException;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-
 /**
- * An encoder that passes encoding request through a chain of
- * {@link EncoderFilter} objects before it gets to the real Encoder.
+ * An encoder that passes encoding request through a chain of {@link EncoderFilter} filters before it gets to the real
+ * Encoder.
  */
 public class FilterChainEncoder implements Encoder {
 
-	private Encoder delegate;
-	private List<EncoderFilter> filters;
+    private Encoder delegate;
+    private List<EncoderFilter> filters;
 
-	public FilterChainEncoder(Encoder delegate, List<EncoderFilter> filters) {
-		this.delegate = delegate;
-		this.filters = filters;
-	}
+    public FilterChainEncoder(Encoder delegate, List<EncoderFilter> filters) {
+        this.delegate = delegate;
+        this.filters = filters;
+    }
 
-	@Override
-	public boolean encode(String propertyName, Object object, JsonGenerator out) throws IOException {
-		return new ChainEncoder().encode(propertyName, object, out);
-	}
+    @Override
+    public boolean encode(String propertyName, Object object, JsonGenerator out) throws IOException {
+        return new ChainEncoder().encode(propertyName, object, out);
+    }
 
-	@Override
-	public boolean willEncode(String propertyName, Object object) {
-		return new ChainEncoder().willEncode(propertyName, object);
-	}
+    @Override
+    public boolean willEncode(String propertyName, Object object) {
+        return new ChainEncoder().willEncode(propertyName, object);
+    }
 
-	private final class ChainEncoder implements Encoder {
-		private int i;
+    private final class ChainEncoder implements Encoder {
+        private int i;
 
-		EncoderFilter nextFilter() {
-			EncoderFilter filter = i >= filters.size() ? null : filters.get(i);
+        EncoderFilter nextFilter() {
+            EncoderFilter filter = i < filters.size() ? filters.get(i) : null;
 
-			i++;
-			return filter;
-		}
+            i++;
+            return filter;
+        }
 
-		@Override
-		public boolean encode(String propertyName, Object object, JsonGenerator out) throws IOException {
-			EncoderFilter filter = nextFilter();
-			if (filter != null) {
-				return filter.encode(propertyName, object, out, this);
-			} else {
-				return delegate.encode(propertyName, object, out);
-			}
-		}
+        @Override
+        public boolean encode(String propertyName, Object object, JsonGenerator out) throws IOException {
+            EncoderFilter filter = nextFilter();
+            return filter != null
+                    ? filter.encode(propertyName, object, out, this)
+                    : delegate.encode(propertyName, object, out);
+        }
 
-		@Override
-		public boolean willEncode(String propertyName, Object object) {
-			EncoderFilter filter = nextFilter();
-			if (filter != null) {
-				return filter.willEncode(propertyName, object, this);
-			} else {
-				return delegate.willEncode(propertyName, object);
-			}
-		}
-	}
-
+        @Override
+        public boolean willEncode(String propertyName, Object object) {
+            EncoderFilter filter = nextFilter();
+            return filter != null
+                    ? filter.willEncode(propertyName, object, this)
+                    : delegate.willEncode(propertyName, object);
+        }
+    }
 }
