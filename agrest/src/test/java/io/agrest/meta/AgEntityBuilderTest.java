e1d9a59014678a8133e7436cf41e7dbd057f579d
Andrus Adamchik <andrus@objectstyle.com>
2019-09-18 17:01:22 +0300

    typo in test name

--- agrest/src/test/java/io/agrest/meta/AgEntityBuilderTest.java
+++ agrest/src/test/java/io/agrest/meta/AgEntityBuilderTest.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
-public class AgjEntityBuilderTest {
+public class AgEntityBuilderTest {
 
 	private static Collection<AgEntityCompiler> COMPILERS;
 
