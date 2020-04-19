382490100384cb01eaca6896cea7ac6ab6c7894e
Andrus Adamchik <andrus@objectstyle.com>
2020-04-19 09:40:35 +0300

    Refactoring - removing Cayenne dependency from generic tests

--- agrest/src/test/java/io/agrest/provider/AgExceptionMapperIT.java
+++ agrest/src/test/java/io/agrest/provider/AgExceptionMapperIT.java
@@ -2,35 +2,29 @@ package io.agrest.provider;
 
 import io.agrest.AgException;
 import io.agrest.DataResponse;
-import io.agrest.it.fixture.JerseyAndDerbyCase;
-import io.agrest.it.fixture.cayenne.E2;
+import io.agrest.it.fixture.JerseyAndPojoCase;
+import io.agrest.it.fixture.pojo.model.P1;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-public class AgExceptionMapperIT extends JerseyAndDerbyCase {
+public class AgExceptionMapperIT extends JerseyAndPojoCase {
 
     @BeforeClass
     public static void startTestRuntime() {
         startTestRuntime(Resource.class);
     }
 
-    @Override
-    protected Class<?>[] testEntities() {
-        return new Class[0];
-    }
-
     @Test
     public void testException() {
-        Response response = target("/g1").request().get();
+        Response response = target("/p1").request().get();
         onResponse(response)
                 .statusEquals(Response.Status.FORBIDDEN)
                 .bodyEquals("{\"success\":false,\"message\":\"_was_forbidden_\"}");
@@ -40,12 +34,9 @@ public class AgExceptionMapperIT extends JerseyAndDerbyCase {
     @Produces(MediaType.APPLICATION_JSON)
     public static class Resource {
 
-        @Context
-        private Configuration config;
-
         @GET
-        @Path("g1")
-        public DataResponse<E2> getE2(@Context UriInfo uriInfo) {
+        @Path("p1")
+        public DataResponse<P1> getE2(@Context UriInfo uriInfo) {
             throw new AgException(Response.Status.FORBIDDEN, "_was_forbidden_");
         }
     }
