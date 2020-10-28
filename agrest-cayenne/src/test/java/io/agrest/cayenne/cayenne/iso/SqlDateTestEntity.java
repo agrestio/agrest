b68f0300255cc9c1be9361bcc8a61dcae42ddf24
Andrus Adamchik <andrus@objectstyle.com>
2020-10-28 16:09:00 +0300

    removing unused "iso" test DataMap

--- agrest-cayenne/src/test/java/io/agrest/cayenne/cayenne/iso/SqlDateTestEntity.java
+++
@@ -1,9 +0,0 @@
-package io.agrest.cayenne.cayenne.iso;
-
-import io.agrest.cayenne.cayenne.iso.auto._SqlDateTestEntity;
-
-public class SqlDateTestEntity extends _SqlDateTestEntity {
-
-    private static final long serialVersionUID = 1L; 
-
-}
