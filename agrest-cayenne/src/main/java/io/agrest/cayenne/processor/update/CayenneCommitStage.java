591f9c28e146e9746dc13274721aa48e9c88ca9a
Andrus Adamchik <andrus@objectstyle.com>
2021-11-28 11:38:01 +0200

    Update processes result twice #504

--- agrest-cayenne/src/main/java/io/agrest/cayenne/processor/update/CayenneCommitStage.java
+++ agrest-cayenne/src/main/java/io/agrest/cayenne/processor/update/CayenneCommitStage.java
@@ -1,21 +1,8 @@
 package io.agrest.cayenne.processor.update;
 
-import io.agrest.AgObjectId;
-import io.agrest.CompoundObjectId;
-import io.agrest.EntityUpdate;
-import io.agrest.NestedResourceEntity;
-import io.agrest.RootResourceEntity;
-import io.agrest.SimpleObjectId;
-import io.agrest.ToManyResourceEntity;
 import io.agrest.processor.Processor;
 import io.agrest.processor.ProcessorOutcome;
 import io.agrest.runtime.processor.update.UpdateContext;
-import org.apache.cayenne.DataObject;
-import org.apache.cayenne.Fault;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Handles {@link io.agrest.UpdateStage#COMMIT} stage of the update process.
@@ -27,51 +14,6 @@ public class CayenneCommitStage implements Processor<UpdateContext<?>> {
     @Override
     public ProcessorOutcome execute(UpdateContext<?> context) {
         CayenneUpdateStartStage.cayenneContext(context).commitChanges();
-
-        // Store parent-child result list in ResourceEntity
-        // TODO: Replace this with a dedicated child mapping stage..
-
-        RootResourceEntity entity = context.getEntity();
-        Map<String, NestedResourceEntity<?>> children = entity.getChildren();
-        List rootResult = new ArrayList();
-        for (EntityUpdate<?> u : context.getUpdates()) {
-            DataObject o = (DataObject) u.getMergedTo();
-            rootResult.add(o);
-            assignChildrenToParent(o, children);
-        }
-
-        entity.setResult(rootResult);
         return ProcessorOutcome.CONTINUE;
-    }
-
-    protected void assignChildrenToParent(DataObject root, Map<String, NestedResourceEntity<?>> children) {
-
-        if (!children.isEmpty()) {
-            for (Map.Entry<String, NestedResourceEntity<?>> e : children.entrySet()) {
-                NestedResourceEntity childEntity = e.getValue();
-
-                Object result = root.readPropertyDirectly(e.getKey());
-                if (result == null || result instanceof Fault) {
-                    continue;
-                }
-
-                AgObjectId id = root.getObjectId().getIdSnapshot().size() > 1
-                        ? new CompoundObjectId(root.getObjectId().getIdSnapshot())
-                        : new SimpleObjectId(root.getObjectId().getIdSnapshot().values().iterator().next());
-
-                if (childEntity instanceof ToManyResourceEntity) {
-                    List r = (List) result;
-
-                    ((ToManyResourceEntity) childEntity).addResultList(id, r);
-                    for (Object ro : r) {
-                        assignChildrenToParent((DataObject) ro, childEntity.getChildren());
-                    }
-
-                } else {
-                    childEntity.addResult(id, result);
-                    assignChildrenToParent((DataObject) result, childEntity.getChildren());
-                }
-            }
-        }
     }
 }
--- agrest-cayenne/src/main/java/io/agrest/cayenne/processor/update/CayenneFillResponseStage.java
+++ agrest-cayenne/src/main/java/io/agrest/cayenne/processor/update/CayenneFillResponseStage.java
@@ -1,15 +1,22 @@
 package io.agrest.cayenne.processor.update;
 
+import io.agrest.AgObjectId;
+import io.agrest.CompoundObjectId;
 import io.agrest.EntityUpdate;
+import io.agrest.NestedResourceEntity;
+import io.agrest.SimpleObjectId;
+import io.agrest.ToManyResourceEntity;
 import io.agrest.processor.Processor;
 import io.agrest.processor.ProcessorOutcome;
 import io.agrest.runtime.processor.update.UpdateContext;
 import org.apache.cayenne.DataObject;
+import org.apache.cayenne.Fault;
 import org.apache.cayenne.ObjectId;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -39,23 +46,58 @@ public abstract class CayenneFillResponseStage implements Processor<UpdateContex
 
         if (context.isIncludingDataInResponse()) {
 
-            // if there are dupes, the list size will be smaller... sizing it
-            // pessimistically
+            // if there are dupes, the list size will be smaller... sizing it pessimistically
             List<T> objects = new ArrayList<>(context.getUpdates().size());
 
-            // 'seen' is for a less common case of multiple updates per object
-            // in a request
+            // 'seen' is for a case of multiple updates per object in a request
             Set<ObjectId> seen = new HashSet<>();
+
+            Map<String, NestedResourceEntity<?>> children = context.getEntity().getChildren();
 
             for (EntityUpdate<T> u : context.getUpdates()) {
 
                 T o = (T) u.getMergedTo();
                 if (o != null && seen.add(o.getObjectId())) {
                     objects.add(o);
+
+                    // TODO: child entities should be seeded via a special NestedDataResolver to read from parent
+                    //  instead of manually traversing objects
+                    assignChildrenToParent(o, children);
                 }
             }
 
             context.getEntity().setResult(objects);
+        }
+    }
+
+    protected void assignChildrenToParent(DataObject root, Map<String, NestedResourceEntity<?>> children) {
+
+        if (!children.isEmpty()) {
+            for (Map.Entry<String, NestedResourceEntity<?>> e : children.entrySet()) {
+                NestedResourceEntity childEntity = e.getValue();
+
+                Object result = root.readPropertyDirectly(e.getKey());
+                if (result == null || result instanceof Fault) {
+                    continue;
+                }
+
+                AgObjectId id = root.getObjectId().getIdSnapshot().size() > 1
+                        ? new CompoundObjectId(root.getObjectId().getIdSnapshot())
+                        : new SimpleObjectId(root.getObjectId().getIdSnapshot().values().iterator().next());
+
+                if (childEntity instanceof ToManyResourceEntity) {
+                    List r = (List) result;
+
+                    ((ToManyResourceEntity) childEntity).addResultList(id, r);
+                    for (Object ro : r) {
+                        assignChildrenToParent((DataObject) ro, childEntity.getChildren());
+                    }
+
+                } else {
+                    childEntity.addResult(id, result);
+                    assignChildrenToParent((DataObject) result, childEntity.getChildren());
+                }
+            }
         }
     }
 }
