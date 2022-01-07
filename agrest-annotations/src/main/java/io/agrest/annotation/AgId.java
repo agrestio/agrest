1c39871869485465eb1568b35b43fa741b6a4595
Andrus Adamchik <andrus@objectstyle.com>
2022-01-07 13:07:19 +0200

    Javadocs

--- agrest-annotations/src/main/java/io/agrest/annotation/AgId.java
+++ agrest-annotations/src/main/java/io/agrest/annotation/AgId.java
@@ -7,7 +7,8 @@ import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Annotates a getter of a property in a POJO to indicate that the property is a Agrest-exposed ID attribute.
+ * Annotates a getter of a property in an object to indicate that it is the entity "id". Multiple methods can be
+ * annotated with "@AgId" in a given entity if a unique identifier of the object id is made of multiple properties.
  * 
  * @since 1.15
  */
