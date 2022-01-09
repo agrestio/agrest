03917774a784d5fbafde0903b2abe1166ee81af4
Andrus Adamchik <andrus@objectstyle.com>
2022-01-09 16:40:50 +0200

    refactoring, no change

--- agrest-engine/src/main/java/io/agrest/provider/SimpleResponseWriter.java
+++ agrest-engine/src/main/java/io/agrest/provider/SimpleResponseWriter.java
@@ -1,9 +1,9 @@
 package io.agrest.provider;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
+import com.fasterxml.jackson.core.JsonGenerator;
+import io.agrest.SimpleResponse;
+import io.agrest.runtime.AgRuntime;
+import io.agrest.runtime.jackson.IJacksonService;
 
 import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Context;
@@ -11,58 +11,59 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
-
-import com.fasterxml.jackson.core.JsonGenerator;
-import io.agrest.SimpleResponse;
-import io.agrest.runtime.AgRuntime;
-import io.agrest.runtime.jackson.IJacksonService;
-import io.agrest.runtime.jackson.JsonConvertable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
 @Provider
 public class SimpleResponseWriter implements MessageBodyWriter<SimpleResponse> {
 
-	private IJacksonService jacksonService;
+    private IJacksonService jacksonService;
 
-	@Context
-	private Configuration configuration;
+    @Context
+    private Configuration configuration;
 
-	@Override
-	public long getSize(SimpleResponse t, Class<?> type, Type genericType, Annotation[] annotations,
-			MediaType mediaType) {
-		return -1;
-	}
+    @Override
+    public long getSize(SimpleResponse t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
 
-	@Override
-	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-		return SimpleResponse.class.isAssignableFrom(type);
-	}
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return SimpleResponse.class.isAssignableFrom(type);
+    }
 
-	@Override
-	public void writeTo(final SimpleResponse t, Class<?> type, Type genericType, Annotation[] annotations,
-			MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
-					throws IOException {
+    @Override
+    public void writeTo(
+            SimpleResponse t,
+            Class<?> type,
+            Type genericType,
+            Annotation[] annotations,
+            MediaType mediaType,
+            MultivaluedMap<String, Object> httpHeaders,
+            OutputStream entityStream) throws IOException {
 
-		getJacksonService().outputJson(new JsonConvertable() {
-			@Override
-			public void generateJSON(JsonGenerator out) throws IOException {
-				out.writeStartObject();
-				out.writeBooleanField("success", t.isSuccess());
+        getJacksonService().outputJson(out -> writeData(t, out), entityStream);
+    }
 
-				if (t.getMessage() != null) {
-					out.writeStringField("message", t.getMessage());
-				}
+    private IJacksonService getJacksonService() {
+        if (jacksonService == null) {
+            jacksonService = AgRuntime.service(IJacksonService.class, configuration);
+        }
 
-				out.writeEndObject();
-			}
-		}, entityStream);
-	}
+        return jacksonService;
+    }
 
-	private IJacksonService getJacksonService() {
-		if (jacksonService == null) {
-			jacksonService = AgRuntime.service(IJacksonService.class, configuration);
-		}
+    protected void writeData(SimpleResponse t, JsonGenerator out) throws IOException {
+        out.writeStartObject();
+        out.writeBooleanField("success", t.isSuccess());
 
-		return jacksonService;
-	}
+        if (t.getMessage() != null) {
+            out.writeStringField("message", t.getMessage());
+        }
+
+        out.writeEndObject();
+    }
 
 }
