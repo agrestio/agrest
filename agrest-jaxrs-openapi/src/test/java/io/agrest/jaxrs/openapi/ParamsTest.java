541e1c34ece052c17fe5c349830871123591f075
Andrus Adamchik <andrus@objectstyle.com>
2022-01-23 09:32:41 +0200

    Split JAX-RS interface to Agrest in a separate module #537
    renaming "agrest-openapi" to "agrest-jaxrs-openapi"

---
+++ agrest-jaxrs-openapi/src/test/java/io/agrest/jaxrs/openapi/ParamsTest.java
@@ -0,0 +1,105 @@
+package io.agrest.jaxrs.openapi;
+
+import io.agrest.DataResponse;
+import org.example.entity.NonAgP1;
+import io.agrest.jaxrs.openapi.junit.TestOpenAPIBuilder;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParamsTest {
+
+    static final OpenAPI oapi = new TestOpenAPIBuilder()
+            .addClass(Resource.class)
+            .addPackage(NonAgP1.class)
+            .build();
+
+    @Test
+    public void testUriInfoParameters() {
+        PathItem pi = oapi.getPaths().get("/r/uri-info");
+        Operation get = pi.getGet();
+
+        assertEquals(9, get.getParameters().size());
+        Map<String, Parameter> paramsMap = new HashMap<>();
+        get.getParameters().forEach(p -> paramsMap.put(p.getName(), p));
+
+        assertEquals(
+                new HashSet<>(asList("cayenneExp", "dir", "exclude", "exp", "include", "limit", "mapBy", "sort", "start")),
+                paramsMap.keySet());
+
+        // TODO assert the actual parameters
+    }
+
+    @Test
+    public void testExplicitParameters() {
+        PathItem pi = oapi.getPaths().get("/r/explicit-params");
+        Operation get = pi.getGet();
+
+        assertEquals(2, get.getParameters().size());
+        Map<String, Parameter> paramsMap = new HashMap<>();
+        get.getParameters().forEach(p -> paramsMap.put(p.getName(), p));
+
+        assertEquals(
+                new HashSet<>(asList("exp", "sort")),
+                paramsMap.keySet());
+
+        // TODO assert the actual parameters
+    }
+
+    @Test
+    public void testMixedParameters() {
+        PathItem pi = oapi.getPaths().get("/r/mixed-params");
+        Operation get = pi.getGet();
+
+        assertEquals(10, get.getParameters().size());
+        Map<String, Parameter> paramsMap = new HashMap<>();
+        get.getParameters().forEach(p -> paramsMap.put(p.getName(), p));
+
+        // TODO: Agrest should define the names enum publicly somewhere
+        assertEquals(
+                new HashSet<>(asList("x", "cayenneExp", "dir", "exclude", "exp", "include", "limit", "mapBy", "sort", "start")),
+                paramsMap.keySet());
+
+        // TODO assert the actual parameters
+    }
+
+    @Path("r")
+    public static class Resource {
+
+        @Context
+        private Configuration config;
+
+        @GET
+        @Path("uri-info")
+        public DataResponse<NonAgP1> uriInfo(@Context UriInfo uriInfo) {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+
+        @GET
+        @Path("explicit-params")
+        public DataResponse<NonAgP1> explicitParams(@QueryParam("exp") String exp, @QueryParam("sort") String sort) {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+
+        @GET
+        @Path("mixed-params")
+        public DataResponse<NonAgP1> mixedParams(@QueryParam("x") String x, @Context UriInfo uriInfo) {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+    }
+}
---
+++ agrest-jaxrs-openapi/src/test/java/io/agrest/jaxrs/openapi/ResponseTest.java
@@ -0,0 +1,154 @@
+package io.agrest.jaxrs.openapi;
+
+import io.agrest.DataResponse;
+import io.agrest.entity.AgP1;
+import io.agrest.jaxrs.openapi.junit.TestOpenAPIBuilder;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.example.entity.NonAgP1;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Configuration;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ResponseTest {
+
+    static final OpenAPI oapi = new TestOpenAPIBuilder()
+            .addClass(Resource.class)
+            .addPackage(NonAgP1.class)
+            .addPackage(AgP1.class)
+            .build();
+
+    @Test
+    public void testDataResponseWithType() {
+        PathItem pi = oapi.getPaths().get("/r/data-response-with-type");
+        ApiResponses responses = pi.getGet().getResponses();
+
+        assertEquals(1, responses.size());
+        ApiResponse response = responses.getDefault();
+        assertNotNull(response);
+
+        assertEquals(1, response.getContent().size());
+        MediaType mt = response.getContent().get("*/*");
+        assertNotNull(mt);
+        Schema schema = mt.getSchema();
+        assertNotNull(schema);
+
+        assertNull(schema.getName());
+        assertNull(schema.getType());
+        assertEquals("#/components/schemas/DataResponse(NonAgP1)", schema.get$ref());
+    }
+
+    @Test
+    public void testDataResponseWithIoAgrestType() {
+        PathItem pi = oapi.getPaths().get("/r/data-response-with-ioagrest-type");
+        ApiResponses responses = pi.getGet().getResponses();
+
+        assertEquals(1, responses.size());
+        ApiResponse response = responses.getDefault();
+        assertNotNull(response);
+
+        assertEquals(1, response.getContent().size());
+        MediaType mt = response.getContent().get("*/*");
+        assertNotNull(mt);
+        Schema schema = mt.getSchema();
+        assertNotNull(schema);
+
+        assertNull(schema.getName());
+        assertNull(schema.getType());
+        assertEquals("#/components/schemas/DataResponse(AgP1)", schema.get$ref());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/r/data-response-bare", "/r/data-response-wildcard"})
+    public void testDataResponseWithNoType(String uri) {
+        PathItem pi = oapi.getPaths().get(uri);
+        ApiResponses responses = pi.getGet().getResponses();
+
+        assertEquals(1, responses.size());
+        ApiResponse response = responses.getDefault();
+        assertNotNull(response);
+
+        assertEquals(1, response.getContent().size());
+        MediaType mt = response.getContent().get("*/*");
+        assertNotNull(mt);
+        Schema schema = mt.getSchema();
+        assertNotNull(schema);
+
+        assertNull(schema.getName());
+        assertNull(schema.getType());
+        assertEquals("#/components/schemas/DataResponse(Object)", schema.get$ref());
+    }
+
+    @Test
+    public void testResponseAnnotated() {
+        PathItem pi = oapi.getPaths().get("/r/response-annotated");
+        ApiResponses responses = pi.getGet().getResponses();
+
+        assertEquals(1, responses.size());
+        ApiResponse response = responses.getDefault();
+        assertNotNull(response);
+
+        assertEquals(1, response.getContent().size());
+        MediaType mt = response.getContent().get("*/*");
+        assertNotNull(mt);
+        Schema schema = mt.getSchema();
+        assertNotNull(schema);
+
+        assertNull(schema.getName());
+        assertNull(schema.getType());
+        assertEquals("#/components/schemas/DataResponse(Object)", schema.get$ref());
+    }
+
+    @Path("r")
+    public static class Resource {
+
+        @Context
+        private Configuration config;
+
+        @GET
+        @Path("data-response-with-type")
+        public DataResponse<NonAgP1> dataResponseWithType() {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+
+        @GET
+        @Path("data-response-with-ioagrest-type")
+        public DataResponse<AgP1> dataResponseWithIOAgrestType() {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+
+        @GET
+        @Path("data-response-bare")
+        public DataResponse dataResponseBare() {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+
+        @GET
+        @Path("data-response-wildcard")
+        public DataResponse<?> dataResponseWildcard() {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+
+        @GET
+        @Path("response-annotated")
+        @Operation(responses = @io.swagger.v3.oas.annotations.responses.ApiResponse(content = @Content(
+                schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = DataResponse.class))))
+        public Response responseAnnotated() {
+            throw new UnsupportedOperationException("endpoint logic is irrelevant for the test");
+        }
+    }
+}
