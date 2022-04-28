37664881d0da008cb603c1ae2a945887968f0686
Ivan-nikitko <70625960+Ivan-nikitko@users.noreply.github.com>
2022-04-28 19:28:14 +0200

    Some tests added, fix errors in test entities

--- agrest-jpa/src/test/java/io/agrest/jpa/POST_IT.java
+++ agrest-jpa/src/test/java/io/agrest/jpa/POST_IT.java
@@ -12,9 +12,12 @@ import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Configuration;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
+import java.util.HashMap;
+import java.util.Map;
 
 public class POST_IT extends DbTest {
 
