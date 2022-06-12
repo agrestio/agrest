2d4546385526f89eaaccd74c73400c6d88a0054c
Andrus Adamchik <andrus@objectstyle.com>
2022-06-12 13:00:59 +0200

    clarifying javadocs

--- agrest-engine/src/main/java/io/agrest/SimpleResponse.java
+++ agrest-engine/src/main/java/io/agrest/SimpleResponse.java
@@ -3,33 +3,33 @@ package io.agrest;
 import io.agrest.protocol.MessageResponse;
 
 /**
- * A response object that represents a 'Simple Document' from Agrest protocol .
+ * Default implementation of {@link MessageResponse}.
  */
 public class SimpleResponse extends AgResponse implements MessageResponse {
 
-	protected final String message;
+    protected final String message;
 
-	/**
-	 * @since 5.0
-	 */
-	public static SimpleResponse of(int status) {
-		return new SimpleResponse(status, null);
-	}
+    /**
+     * @since 5.0
+     */
+    public static SimpleResponse of(int status) {
+        return new SimpleResponse(status, null);
+    }
 
-	/**
-	 * @since 5.0
-	 */
-	public static SimpleResponse of(int status, String message) {
-		return new SimpleResponse(status, message);
-	}
+    /**
+     * @since 5.0
+     */
+    public static SimpleResponse of(int status, String message) {
+        return new SimpleResponse(status, message);
+    }
 
-	protected SimpleResponse(int status, String message) {
-		super(status);
-		this.message = message;
-	}
+    protected SimpleResponse(int status, String message) {
+        super(status);
+        this.message = message;
+    }
 
-	@Override
-	public String getMessage() {
-		return message;
-	}
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
