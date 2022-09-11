1fbfdbf33f96946bca7979d13872bea7c9a14404
Andrus Adamchik <andrus@objectstyle.com>
2022-09-11 13:02:58 +0200

    Better default encoder for DataResponse #577
    tests using a raw DataResponse

--- agrest-engine/src/test/java/io/agrest/runtime/jackson/JacksonServiceTest.java
+++ agrest-engine/src/test/java/io/agrest/runtime/jackson/JacksonServiceTest.java
@@ -3,9 +3,21 @@ package io.agrest.runtime.jackson;
 import io.agrest.AgException;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JacksonServiceTest {
+
+    @Test
+    public void testOutputJson() throws IOException {
+        JacksonService service = new JacksonService();
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        service.outputJson(o -> o.writeObject(new X(1, "two")), out);
+        assertEquals("{\"a\":1,\"b\":\"two\"}", out.toString());
+    }
 
     @Test
     public void testParse_BadJson() {
@@ -19,6 +31,24 @@ public class JacksonServiceTest {
             assertEquals("Error parsing JSON", e.getMessage());
             assertEquals(400, e.getStatus());
             assertNotNull(e.getCause());
+        }
+    }
+
+    public static class X {
+        private final int a;
+        private final String b;
+
+        public X(int a, String b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int getA() {
+            return a;
+        }
+
+        public String getB() {
+            return b;
         }
     }
 }
