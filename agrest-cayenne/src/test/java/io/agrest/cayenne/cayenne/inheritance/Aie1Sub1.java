e0447a2ee70425efaa4e6d9f5cd481acabd1baeb
Andrus Adamchik <andrus@objectstyle.com>
2022-11-02 08:36:35 +0100

    Inheritance-aware AgEntityOverlay #587
    * testing annotated entities in the inheritance hierarchy

---
+++ agrest-cayenne/src/test/java/io/agrest/cayenne/cayenne/inheritance/Aie1Sub1.java
@@ -0,0 +1,13 @@
+package io.agrest.cayenne.cayenne.inheritance;
+
+import io.agrest.annotation.AgAttribute;
+import io.agrest.cayenne.cayenne.inheritance.auto._Aie1Sub1;
+
+public class Aie1Sub1 extends _Aie1Sub1 {
+
+    @AgAttribute(readable = true)
+    @Override
+    public String getA0() {
+        return super.getA0();
+    }
+}
