8f256085465a5151ecd5eeb74990220ef8bba54a
Max Novik <maksim.novik@lucidworks.com>
2022-11-10 18:00:49 +0300

    Polishing.

--- agrest-spring-boot-starter/src/main/java/io/agrest/spring/starter/AgrestAutoConfiguration.java
+++ agrest-spring-boot-starter/src/main/java/io/agrest/spring/starter/AgrestAutoConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @Configuration
 @ConditionalOnWebApplication
--- agrest-spring-boot-starter/src/main/java/io/agrest/spring/starter/exceptions/ApiError.java
+++ agrest-spring-boot-starter/src/main/java/io/agrest/spring/starter/exceptions/ApiError.java
@@ -1,7 +1,5 @@
 package io.agrest.spring.starter.exceptions;
 
-import org.springframework.http.HttpStatus;
-
 public class ApiError {
 
   private final String message;
