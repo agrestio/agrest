f2976198dd7c0de5fbc68c0d2eafb7099fdf6fe8
Andrus Adamchik <andrus@objectstyle.com>
2023-07-08 19:34:05 +0200

    Cayenne IPathResolver may get "poisoned" by per-request overlayed AgEntities #641
    extra tests

--- agrest-cayenne/src/test/java/io/agrest/cayenne/GET/Exp_PropFilter_Overlay_RequestIT.java
+++ agrest-cayenne/src/test/java/io/agrest/cayenne/GET/Exp_PropFilter_Overlay_RequestIT.java
@@ -6,6 +6,7 @@ import io.agrest.cayenne.cayenne.main.E2;
 import io.agrest.cayenne.unit.main.MainDbTest;
 import io.agrest.cayenne.unit.main.MainModelTester;
 import io.agrest.jaxrs3.AgJaxrs;
+import io.agrest.meta.AgEntity;
 import io.bootique.junit5.BQTestTool;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -14,17 +15,21 @@ import jakarta.ws.rs.core.Configuration;
 import jakarta.ws.rs.core.Context;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class Exp_PropFilter_Overlay_RequestIT extends MainDbTest {
 
+    // testing condition described in https://github.com/agrestio/agrest/issues/641
+    // running in "TEST_METHOD" scope to reset the runtime from possible cache poisoning
+
     @BQTestTool
-    static final MainModelTester tester = tester(Resource.class)
+    final MainModelTester tester = tester(Resource.class)
             .entitiesAndDependencies(E2.class)
             .build();
 
     @Test
-    public void narrowThanWide() {
-
-        // testing condition described in https://github.com/agrestio/agrest/issues/641
+    public void noNameThanName() {
 
         tester.e2()
                 .insertColumns("id_", "name", "address")
@@ -44,6 +49,29 @@ public class Exp_PropFilter_Overlay_RequestIT extends MainDbTest {
                 .get()
                 .wasOk()
                 .bodyEquals(1, "{\"id\":1,\"address\":\"a1\",\"name\":\"n1\"}");
+    }
+
+    @Test
+    public void nameAsDateThenAsString() {
+
+        tester.e2()
+                .insertColumns("id_", "name", "address")
+                .values(1, "2021-01-01", "a1")
+                .values(2, "2021-01-02", "a2")
+                .exec();
+
+        tester.target("e2-name-as-date")
+                // TODO: is this correct - filtering on a redefined attribute?
+                .queryParam("exp", "name = '2021-01-01'")
+                .get()
+                .wasOk()
+                .bodyEquals(1, "{\"id\":1,\"address\":\"a1\",\"name\":\"2021-01-01\"}");
+
+        tester.target("e2-all")
+                .queryParam("exp", "name = '2021-01-01'")
+                .get()
+                .wasOk()
+                .bodyEquals(1, "{\"id\":1,\"address\":\"a1\",\"name\":\"2021-01-01\"}");
     }
 
     @Path("")
@@ -66,6 +94,23 @@ public class Exp_PropFilter_Overlay_RequestIT extends MainDbTest {
             return AgJaxrs.select(E2.class, config)
                     .request(request)
                     .propFilter(E2.class, b -> b.property("name", false))
+                    .get();
+        }
+
+        @GET
+        @Path("e2-name-as-date")
+        public DataResponse<E2> nameAsDate(@QueryParam("exp") String exp) {
+            AgRequest request = AgJaxrs.request(config)
+                    .andExp(exp)
+                    .build();
+
+            return AgJaxrs.select(E2.class, config)
+                    .request(request)
+                    .entityOverlay(AgEntity.overlay(E2.class).attribute(
+                            "name",
+                            // using SQL date for tests to match an existing converter in CayenneExpPostProcessor
+                            Date.class,
+                            e2 -> Date.valueOf(LocalDate.parse(e2.getName()))))
                     .get();
         }
     }
