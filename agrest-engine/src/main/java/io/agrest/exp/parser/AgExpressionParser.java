9944b864e5a3217d13843e85902287c020817ed3
Mikhail Dzianishchyts <mikhail.dzianishchyts@gmail.com>
2023-07-24 18:39:45 +0300

    Parse 'or' and 'and' nodes with multiple children

--- agrest-engine/src/main/java/io/agrest/exp/parser/AgExpressionParser.java
+++ agrest-engine/src/main/java/io/agrest/exp/parser/AgExpressionParser.java
@@ -32,84 +32,84 @@ return (AgExpression) jjtree.rootNode();
 }
 
   final public void orCondition() throws ParseException {
-    andCondition();
-    label_1:
-    while (true) {
-      switch (jj_nt.kind) {
-      case 1:{
-        ;
-        break;
-        }
-      default:
-        jj_la1[0] = jj_gen;
-        break label_1;
-      }
-      jj_consume_token(1);
 ExpOr jjtn001 = new ExpOr(JJTOR);
-                                boolean jjtc001 = true;
-                                jjtree.openNodeScope(jjtn001);
-      try {
+          boolean jjtc001 = true;
+          jjtree.openNodeScope(jjtn001);
+    try {
+      andCondition();
+      label_1:
+      while (true) {
+        switch (jj_nt.kind) {
+        case 1:{
+          ;
+          break;
+          }
+        default:
+          jj_la1[0] = jj_gen;
+          break label_1;
+        }
+        jj_consume_token(1);
         andCondition();
-      } catch (Throwable jjte001) {
-if (jjtc001) {
-                                  jjtree.clearNodeScope(jjtn001);
-                                  jjtc001 = false;
-                                } else {
-                                  jjtree.popNode();
-                                }
-                                if (jjte001 instanceof RuntimeException) {
-                                  {if (true) throw (RuntimeException)jjte001;}
-                                }
-                                if (jjte001 instanceof ParseException) {
-                                  {if (true) throw (ParseException)jjte001;}
-                                }
-                                {if (true) throw (Error)jjte001;}
-      } finally {
-if (jjtc001) {
-                                  jjtree.closeNodeScope(jjtn001,  2);
-                                }
       }
+    } catch (Throwable jjte001) {
+if (jjtc001) {
+            jjtree.clearNodeScope(jjtn001);
+            jjtc001 = false;
+          } else {
+            jjtree.popNode();
+          }
+          if (jjte001 instanceof RuntimeException) {
+            {if (true) throw (RuntimeException)jjte001;}
+          }
+          if (jjte001 instanceof ParseException) {
+            {if (true) throw (ParseException)jjte001;}
+          }
+          {if (true) throw (Error)jjte001;}
+    } finally {
+if (jjtc001) {
+            jjtree.closeNodeScope(jjtn001, jjtree.nodeArity() > 1);
+          }
     }
 }
 
   final public void andCondition() throws ParseException {
-    notCondition();
-    label_2:
-    while (true) {
-      switch (jj_nt.kind) {
-      case 2:{
-        ;
-        break;
-        }
-      default:
-        jj_la1[1] = jj_gen;
-        break label_2;
-      }
-      jj_consume_token(2);
 ExpAnd jjtn001 = new ExpAnd(JJTAND);
-                                 boolean jjtc001 = true;
-                                 jjtree.openNodeScope(jjtn001);
-      try {
+          boolean jjtc001 = true;
+          jjtree.openNodeScope(jjtn001);
+    try {
+      notCondition();
+      label_2:
+      while (true) {
+        switch (jj_nt.kind) {
+        case 2:{
+          ;
+          break;
+          }
+        default:
+          jj_la1[1] = jj_gen;
+          break label_2;
+        }
+        jj_consume_token(2);
         notCondition();
-      } catch (Throwable jjte001) {
-if (jjtc001) {
-                                   jjtree.clearNodeScope(jjtn001);
-                                   jjtc001 = false;
-                                 } else {
-                                   jjtree.popNode();
-                                 }
-                                 if (jjte001 instanceof RuntimeException) {
-                                   {if (true) throw (RuntimeException)jjte001;}
-                                 }
-                                 if (jjte001 instanceof ParseException) {
-                                   {if (true) throw (ParseException)jjte001;}
-                                 }
-                                 {if (true) throw (Error)jjte001;}
-      } finally {
-if (jjtc001) {
-                                   jjtree.closeNodeScope(jjtn001,  2);
-                                 }
       }
+    } catch (Throwable jjte001) {
+if (jjtc001) {
+            jjtree.clearNodeScope(jjtn001);
+            jjtc001 = false;
+          } else {
+            jjtree.popNode();
+          }
+          if (jjte001 instanceof RuntimeException) {
+            {if (true) throw (RuntimeException)jjte001;}
+          }
+          if (jjte001 instanceof ParseException) {
+            {if (true) throw (ParseException)jjte001;}
+          }
+          {if (true) throw (Error)jjte001;}
+    } finally {
+if (jjtc001) {
+            jjtree.closeNodeScope(jjtn001, jjtree.nodeArity() > 1);
+          }
     }
 }
 
