069ab2bb06faf970e806be051e94c79a46696563
Andrus Adamchik <andrus@objectstyle.com>
2025-03-22 15:58:45 -0400

    using modern BQ API for tests

--- agrest-jaxrs2/src/test/java/io/agrest/jaxrs2/junit/AgPojoTester.java
+++ agrest-jaxrs2/src/test/java/io/agrest/jaxrs2/junit/AgPojoTester.java
@@ -15,7 +15,7 @@ import io.agrest.runtime.AgRuntimeBuilder;
 import io.bootique.BQRuntime;
 import io.bootique.Bootique;
 import io.bootique.command.CommandOutcome;
-import io.bootique.di.BQModule;
+import io.bootique.BQModule;
 import io.bootique.di.Binder;
 import io.bootique.di.Provides;
 import io.bootique.jersey.JerseyModule;
